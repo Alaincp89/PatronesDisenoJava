c889ccf5e90545fb4c42a1228f2be3d0cb8da5a4
Ing. Alain Cervantes <113310284+Alaincp89@users.noreply.github.com>
2023-05-19 11:20:50 -0500

    Update LinkedIn.java

--- src/Comportamiento/iterator/example/redes_sociales/LinkedIn.java
+++ src/Comportamiento/iterator/example/redes_sociales/LinkedIn.java
@@ -19,24 +19,18 @@ public class LinkedIn implements RedSocial{
     }
 
     public Profile requestContactInfoFromLinkedInAPI(String profileEmail) {
-        // Here would be a POST request to one of the LinkedIn API endpoints.
-        // Instead, we emulates long network connection, which you would expect
-        // in the real life...
         simulateNetworkLatency();
-        System.out.println("LinkedIn: Loading profile '" + profileEmail + "' over the network...");
-
-        // ...and return test data.
+        System.out.println("LinkedIn: Cargando perfil '" + profileEmail + "' a la red...");
+        
         return findContact(profileEmail);
     }
 
     public List<String> requestRelatedContactsFromLinkedInAPI(String profileEmail, String contactType) {
-        // Here would be a POST request to one of the LinkedIn API endpoints.
-        // Instead, we emulates long network connection, which you would expect
-        // in the real life.
+       
         simulateNetworkLatency();
-        System.out.println("LinkedIn: Loading '" + contactType + "' list of '" + profileEmail + "' over the network...");
+        System.out.println("LinkedIn: Cargando '" + contactType + "' Lista de '" + profileEmail + "' a la red...");
 
-        // ...and return test data.
+        
         Profile profile = findContact(profileEmail);
         if (profile != null) {
             return profile.getContacts(contactType);
@@ -63,11 +57,11 @@ public class LinkedIn implements RedSocial{
 
     @Override
     public ProfileIterator createFriendsIterator(String profileEmail) {
-        return new LinkedInIterator(this, "friends", profileEmail);
+        return new LinkedInIterator(this, "Amigos", profileEmail);
     }
 
     @Override
     public ProfileIterator createCoworkersIterator(String profileEmail) {
-        return new LinkedInIterator(this, "coworkers", profileEmail);
+        return new LinkedInIterator(this, "Compañeros de trabajo", profileEmail);
     }
 }
